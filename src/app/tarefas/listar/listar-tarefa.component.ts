186279727503d75d0b71b2a92e7c1889f7917981
Buarque <thales.de.b.buarque@accenture.com>
2019-09-26 18:01:51 -0300

    Task List Components and Services

---
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+import { TarefaService, Tarefa } from '../shared';
+
+@Component({
+  selector: 'app-listar-tarefa',
+  templateUrl: './listar-tarefa.component.html',
+  styleUrls: ['./listar-tarefa.component.css']
+})
+export class ListarTarefasComponent implements OnInit {
+
+  tarefas: Tarefa[];
+
+  constructor(
+    private tarefaService: TarefaService
+  ) { }
+
+  ngOnInit() {
+    this.tarefas = this.listarTodos();
+    this.tarefas = [
+      new Tarefa(1,'Tarefa 1', false),
+      new Tarefa(2,'Tarefa 2', true)
+    ];
+  }
+
+  listarTodos(): Tarefa[] {
+    return this.tarefaService.listarTodos();
+  }
+
+}
--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {TarefaService} from './shared';
-
+import { ListarTarefasComponent } from './listar';
+import {RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ListarTarefasComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [
     TarefaService
